d1343bf3d9af61cf2fe445c55bca80b49daf400d
StenkinVlad <50441898+StenkinVlad@users.noreply.github.com>
2021-01-24 12:11:52 +0300

    yaeh baby!

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('13', views.page13),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+
 # status = 1 - only view the posted posts, ton draft
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -11,3 +12,12 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+# from django.http import HttpResponse
+# def page13(request):
+#     # template_name = '13.html'
+#     return HttpResponse ('this is page 13')
+
+# from django.http import HttpResponse
+def page13(request):
+    return render (request, '13.html')
