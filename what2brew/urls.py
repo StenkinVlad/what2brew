a1af4fe6464b502aa329d361dfb3b812262c8239
StenkinVlad <50441898+StenkinVlad@users.noreply.github.com>
2021-03-12 19:14:19 +0300

    Update urls.py

--- what2brew/urls.py
+++ what2brew/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
-    # path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+    # path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     ]
 
 if settings.DEBUG:
